242d8dd3f1c1b412476dc37fefd019cd7479ed73
Tom Smith <wisdev53548@hotmail.com>
2018-02-19 20:52:31 -0600

    Added dynamic state property

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
+  state = {
+    people: [
+      {name: 'Jack', age: (Math.floor(Math.random() * 20) + 1)},
+      {name: 'Ethan', age: (Math.floor(Math.random() * 20) + 1)},
+      {name: 'Michael', age: (Math.floor(Math.random() * 20) + 1)}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Company Roster</h1>
-        <Person name="Mark" />
-        <Person name="Joe">I love the company</Person>
-        <Person name="Sarah" />
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>I will be the CEO</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const person = props => {
   return (
     <div>
-      <h2>{props.name} has been with the company for {(Math.floor(Math.random() * 20) + 1)} years.</h2>
+      <h2>{props.name} has been with the company for {props.age} years.</h2>
       <p>{props.children}</p>
     </div>
   )
