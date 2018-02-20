1d5577eae574e86ce633088c4a3211350b94a401
Tom Smith <wisdev53548@hotmail.com>
2018-02-19 21:15:16 -0600

    Added nameChangeHandler

--- src/App.js
+++ src/App.js
@@ -11,10 +11,21 @@ class App extends Component {
     ]
   }
 
+  nameChangeHandler = () => {
+    this.setState({
+      people: [
+        {name: 'Braden', yearsAtCompany: (Math.floor(Math.random() * 20) + 1)},
+        {name: 'Josh', yearsAtCompany: (Math.floor(Math.random() * 20) + 1)},
+        {name: 'Kevin', yearsAtCompany: (Math.floor(Math.random() * 20) + 1)}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Company Roster</h1>
+        <button onClick={this.nameChangeHandler}>Change Names</button>
         <Person name={this.state.people[0].name} yearsAtCompany={this.state.people[0].yearsAtCompany} />
         <Person name={this.state.people[1].name} yearsAtCompany={this.state.people[1].yearsAtCompany}>Currently on leave.</Person>
         <Person name={this.state.people[2].name} yearsAtCompany={this.state.people[2].yearsAtCompany} />
